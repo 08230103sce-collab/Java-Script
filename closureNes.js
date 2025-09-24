8ab67553037362bb728fa1ff97eb678865c1b77b
08230103sce-collab <08230103.sce@rub.edu.bt>
2025-09-24 10:41:00 +0600

    modify  types of functions

---
+++ closureNes.js
@@ -0,0 +1,16 @@
+function outerFun (a) {
+    console.log("The value of a is: "+1);
+    function innerFun(b){
+        console.log(" The value of b is:"+b);
+        return a+b;
+    }
+
+        return innerFun;
+}
+
+
+const addTen = outerFun(10);    // addTen is now innerFun with a = 10
+console.log(addTen(5)); // Outputs 15 (10 + 5)
+
+
+    
